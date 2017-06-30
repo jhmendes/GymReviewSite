1c52cedb932bf7c6497827994182464a96d388f7
John Mendes <johnhenrymendes@gmail.comgit config --global user.name John>
2017-06-30 13:30:31 -0400

    created database and installed dependencies

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ Bundler.require(*Rails.groups)
 module GymReviewSite
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    # config.load_defaults 5.1
+    # commented this out, is this bad?
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
