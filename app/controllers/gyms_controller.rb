d20d58c62318279bd875036d18c1e503c2c3b1d3
John Mendes <johnhenrymendes@gmail.comgit config --global user.name John>
2017-07-04 11:43:52 -0400

    added comments on gyms controller for user authentication

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,6 +1,11 @@
 class GymsController < ApplicationController
 
+# before_action :authenticate_user!, except: [:index, :show]
+# unauthenticated users may only VIEW the gyms index and invidual show pages
+# they cannot create, edit, or delete gyms
+# this should be the same for reviews
+
   def index
-    
+
   end
 end
